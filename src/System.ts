014d1b6766fff94cb8776678787abe2546d35f76
Erlin Emrys <Erlin@seznam.cz>
2021-05-03 21:11:13 +0100

    Build fixed 7

--- src/System.ts
+++ src/System.ts
@@ -11,7 +11,7 @@
  */
 
 // Import TypeScript modules
-import { registerSettings } from "./module/Something";
+import { registerSettings } from "./module/Settings";
 import { preloadTemplates } from './module/PreloadTemplates';
 
 /* ------------------------------------ */
