9132fd7f387a5d082d73b787b048c40f9735919f
Erlin Emrys <Erlin@seznam.cz>
2021-05-03 18:25:44 +0100

    BF23

--- src/module/preloadTemplates.ts
+++ src/module/preloadTemplates.ts
@@ -1,8 +1,9 @@
-export const preloadTemplates = async function() {
+/*export const preloadTemplates = async function() {
 	const templatePaths = [
 		// Add paths to "systems/DrDPlus/templates"
 		"systems/DrDPlus/templates/parts/DrdAttributes.html",
 	];
 
 	return loadTemplates(templatePaths);
-}
+}*/
+export const TEST = ["String", "Number", "Boolean", "Formula", "Resource"];
