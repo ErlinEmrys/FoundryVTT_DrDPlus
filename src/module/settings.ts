39d2623b990a62172a1fc2cba813fae07da0d95b
Erlin Emrys <Erlin@seznam.cz>
2021-05-03 19:21:59 +0100

    BF29

--- src/module/settings.ts
+++ src/module/settings.ts
@@ -3,7 +3,7 @@ import { DrdActorSheet } from "./actors/DrdActorSheet";
 import { DrdItem } from "./items/DrdItem";
 import { DrdItemSheet } from "./items/DrdItemSheet";
 import { ATTRIBUTE_TYPES } from "./Constants";
-import { preloadTemplates } from "./Constants";
+import { preloadTemplates } from "./PreloadTemplates";
 
 export const registerSettings = function() {
 	
