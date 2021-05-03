cdcbcc18827e5f5fdcaac9d69dae797528e13101
Erlin Emrys <Erlin@seznam.cz>
2021-05-03 18:11:27 +0100

    BF16

--- src/module/preloadTemplates.ts
+++ src/module/preloadTemplates.ts
@@ -1,8 +1,11 @@
-export const preloadTemplates = async function() {/*
+export class DrdTemplate
+{
+	static preloadTemplates() {/*
 	const templatePaths = [
 		// Add paths to "systems/DrDPlus/templates"
 		"systems/DrDPlus/templates/parts/DrdAttributes.html",
 	];
 
 	return loadTemplates(templatePaths);*/
+	}
 }
